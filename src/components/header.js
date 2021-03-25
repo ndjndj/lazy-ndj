93cc417e6e8b7299b833eb98e0011d042acdb5e1
ndjndj <teq.ndj@gmail.com>
2021-03-26 01:28:23 +0900

    header.js

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,21 @@ import React from 'react';
 const Header = () => {
     return (
         <header>
-            
+            <h1>lazy-ndj</h1>
+            <ul>
+                <li>
+                    <a>Blog</a>
+                </li>
+                <li>
+                    <a>Products</a>
+                </li>
+                <li>
+                    <a>About</a>
+                </li>
+                <li>
+                    <a>Contact</a>
+                </li>
+            </ul>
         </header>
     );
 }
