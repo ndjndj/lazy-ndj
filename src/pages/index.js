8050c807a784cec7edb0cff998302fa08a31de43
ndjndj <teq.ndj@gmail.com>
2021-03-26 01:29:14 +0900

    index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
+import Header from '../components/header';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
 }
