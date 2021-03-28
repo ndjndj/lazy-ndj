6ca8bdfc9091971b466bd1e20d4acc4bb87e37b8
ndjndj <teq.ndj@gmail.com>
2021-03-28 16:50:56 +0900

    test

--- src/components/header.js
+++ src/components/header.js
@@ -1,24 +1,13 @@
 import React from 'react';
+import { AppBar, Toolbar } from '@material-ui/core';
 
 const Header = () => {
     return (
-        <header>
-            <h1>lazy-ndj</h1>
-            <ul>
-                <li>
-                    <a>Blog</a>
-                </li>
-                <li>
-                    <a>Products</a>
-                </li>
-                <li>
-                    <a>About</a>
-                </li>
-                <li>
-                    <a>Contact</a>
-                </li>
-            </ul>
-        </header>
+        <AppBar position="static">
+            <Toolbar>
+                <h1>TEST</h1>
+            </Toolbar>
+        </AppBar>
     );
 }
 
