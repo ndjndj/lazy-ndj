3e04214601379b78bcfd5f8826e3f2421ecee3cf
ndjndj <teq.ndj@gmail.com>
2021-03-29 05:43:34 +0900

    header

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, AppBar, Toolbar, List, ListItem, ListItemText } from '@material-ui/core';
+import { makeStyles, AppBar, Toolbar, List, ListItem, ListItemText, Container } from '@material-ui/core';
 import IconRay from '../assets/ray-32.svg';
 
 const useStyles = makeStyles(
@@ -28,21 +28,26 @@ const Header = () => {
     return (
         <AppBar position="static">
             <Toolbar>
-                <strong>
-                    <div>LazyRay</div>
-                </strong>
-                <IconRay />
-                <List component="nav" aria-labelledby="main navigation" className={classes.navDisplayFlex}>
-                    {navLinks.map(
-                        ({title, path}) => (
-                            <a href={path} key={title} className={classes.linkText}>
-                                <ListItem button>
-                                    <ListItemText primary={title} />
-                                </ListItem>
-                            </a>
-                        )
-                    )}
-                </List>
+                <Container className={classes.navDisplayFlex}>
+                    <div>
+                        <strong>
+                            <div>LazyRay</div>
+                        </strong>
+                        <IconRay />
+                    </div>
+                    <List component="nav" aria-labelledby="main navigation" className={classes.navDisplayFlex}>
+                        {navLinks.map(
+                            ({title, path}) => (
+                                <a href={path} key={title} className={classes.linkText}>
+                                    <ListItem button>
+                                        <ListItemText primary={title} />
+                                    </ListItem>
+                                </a>
+                            )
+                        )}
+                    </List>
+                </Container>
+
             </Toolbar>
         </AppBar>
     );
