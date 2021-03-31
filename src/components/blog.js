939a1316ec30528883267d4b4eb702bda80bef0a
ndjndj <teq.ndj@gmail.com>
2021-03-31 21:05:30 +0900

    style

--- src/components/blog.js
+++ src/components/blog.js
@@ -5,3 +5,17 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 
+const useStyles = makeStyles(
+    (theme) => ({
+        markdown: {
+            ...theme.typography.body2,
+            padding: theme.spacing(3, 0),
+        },
+    })
+);
+
+const Main = () => {
+
+}
+
+export default Main;
