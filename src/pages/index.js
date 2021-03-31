7b1a7eeb212dd164e78cb1faff418c8b6fd962e3
ndjndj <teq.ndj@gmail.com>
2021-03-31 22:34:30 +0900

    import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from '../components/header';
 import Blog from '../components/blog';
+import FeatuerdCard from '../components/featuredCard';
 
 export default function Home() {
   return (
     <React.Fragment>
       <Header />
+      <FeatuerdCard />
       <Blog />
     </React.Fragment>
   );
