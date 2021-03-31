eba2a8c9182b9f5d20fc6d4b92af242be795eaf7
ndjndj <teq.ndj@gmail.com>
2021-03-31 22:26:55 +0900

    style

--- src/components/featuredCard.js
+++ src/components/featuredCard.js
@@ -6,4 +6,33 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
 
-
+const useStyles = makeStyles(
+    (theme) => ({
+        mainFeaturedPost: {
+            position: `relative`,
+            backgroundColor: theme.pallete.gray[800],
+            color: theme.pallete.common.white,
+            marginBottom: theme.spacing(4),
+            backgroundImage: `url(https://source.unsplash.com/random)`,
+            backgroundSize: `cover`,
+            backgroundRepeat: `no-repeat`,
+            backgroundPosition: `center`,
+        },
+        overlay: {
+            position: `absolute`,
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            backgroundColor: `rgba(0,0,0,.3)`,
+        },
+        mainFeaturedPostContent: {
+            position: 'relative',
+            padding: theme.spacing(3),
+            [theme.breakpoints.up('md')]: {
+              padding: theme.spacing(6),
+              paddingRight: 0,
+            },
+          },
+    })
+);
