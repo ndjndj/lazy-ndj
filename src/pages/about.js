523ca8cfa858e2290d0f3f91f346a79c8dea9066
ndjndj <teq.ndj@gmail.com>
2021-04-01 00:46:43 +0900

    about

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Header from '../components/header';
+import Blog from '../components/blog';
+import FeatuerdCard from '../components/featuredCard';
+import FeaturedCardParts from '../components/featuredCardParts';
+import Sidebar from '../components/sidebar';
+
+export default function Home() {
+  return (
+    <React.Fragment>
+      <Header />
+      <FeatuerdCard />
+      <FeaturedCardParts />
+      <FeaturedCardParts />
+      <Sidebar />
+    </React.Fragment>
+  );
+}
