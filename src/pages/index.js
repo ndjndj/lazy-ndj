9c6bd91fb4c67c9e37bea929b568a1d16df978cd
ndjndj <teq.ndj@gmail.com>
2021-03-31 21:08:17 +0900

    blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from '../components/header';
+import Blog from '../components/blog';
 
 export default function Home() {
   return (
     <React.Fragment>
       <Header />
+      <Blog />
     </React.Fragment>
   );
 }
