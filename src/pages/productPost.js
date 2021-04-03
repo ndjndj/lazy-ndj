c07d334692b9fe233ad5475418f809e994e4ef3f
ndjndj <teq.ndj@gmail.com>
2021-04-03 23:19:13 +0900

    productPost

---
+++ src/pages/productPost.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Header from '../components/header';
+import ProductCard from '../components/productCard';
+import Sidebar from '../components/sidebar';
+
+const ProductPost = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Sidebar />
+    </React.Fragment>
+  );
+}
+
+export default ProductPost;
