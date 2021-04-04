15bbb4e698e97cb272b9727cc47f4621a82068bd
ndjndj <teq.ndj@gmail.com>
2021-04-04 21:10:12 +0900

    fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Header from '../components/header';
-import Blog from '../components/blog';
 import FeatuerdCard from '../components/featuredCard';
 import FeaturedCardParts from '../components/featuredCardParts';
 import Sidebar from '../components/sidebar';
@@ -9,9 +8,6 @@ export default function About() {
   return (
     <React.Fragment>
       <Header />
-      <FeatuerdCard />
-      <FeaturedCardParts />
-      <FeaturedCardParts />
       <Sidebar />
     </React.Fragment>
   );
