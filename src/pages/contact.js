2adc99efe755d5fd6a789702d3a630bfcdc2404b
ndjndj <teq.ndj@gmail.com>
2021-04-04 21:10:27 +0900

    fix

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,11 @@
 import React from "react"
 import Header from '../components/header';
-import Blog from '../components/blog';
-import FeatuerdCard from '../components/featuredCard';
-import FeaturedCardParts from '../components/featuredCardParts';
 import Sidebar from '../components/sidebar';
 
 export default function Contact() {
   return (
     <React.Fragment>
       <Header />
-      <FeatuerdCard />
-      <FeaturedCardParts />
-      <FeaturedCardParts />
       <Sidebar />
     </React.Fragment>
   );
