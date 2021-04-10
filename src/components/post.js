0b9b494195092e00006e99cefd10f6c211cedd2a
ndjndj <teq.ndj@gmail.com>
2021-04-10 19:54:30 +0900

    skelton post.js

---
+++ src/components/post.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from '../components/header';
+import Sidebar from '../components/sidebar';
+
+const Post = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Sidebar />
+    </React.Fragment>
+  );
+}
+
+export default Post;
