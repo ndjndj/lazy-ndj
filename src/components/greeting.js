d85b72f3fa43a5a15d86065d959949d45cfc01a4
ndjndj <teq.ndj@gmail.com>
2021-04-15 18:18:37 +0900

    skelton

---
+++ src/components/greeting.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Greeting = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Form />
+      <Sidebar />
+    </React.Fragment>
+  );
+}
+
+export default Greeting;
