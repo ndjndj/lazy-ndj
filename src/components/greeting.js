2d10278c0466e0947e80a893812513a567cfb00f
ndjndj <teq.ndj@gmail.com>
2021-04-18 18:52:36 +0900

    age

--- src/components/greeting.js
+++ src/components/greeting.js
@@ -1,12 +1,18 @@
 import React from "react"
 
 const Greeting = () => {
+  const birth = new Date('1994/06/17 02:03:00');
+  const now = new Date();
+  const diff = Math.floor((now.getTime() -birth.getTime()) / (1000 * 60 * 60 * 24) / 365.25);
+
   return (
     <React.Fragment>
             <h1>
         ndj
       </h1>
       <p>はじめまして、ndjです</p>
+
+      <p>1994年生まれの{diff}歳です。</p>
     </React.Fragment>
   );
 }
