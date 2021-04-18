fe8bbd4268154da60070fa0ec99a8483e64b816c
ndjndj <teq.ndj@gmail.com>
2021-04-18 19:02:36 +0900

    fix

--- src/common/common.js
+++ src/common/common.js
@@ -1,7 +1,7 @@
 
 
-export const calcYear = (milliSec) => {
-    const dayMilliSec = 1000 * 60 * 60 * 24;
+export const calcMilliSecToYear = (milliSec) => {
+    const dayMilliSec = 1000 * 60 * 60 * 24; //一日の秒数
     const year = 365.25; // うるう年を考慮
 
     return Math.floor((milliSec) / (dayMilliSec) / year);
@@ -10,3 +10,5 @@ export const calcYear = (milliSec) => {
 export const calcDateDiffMilliSec = (fromTime, toTime) => {
     return fromTime.getTime() - toTime.getTime();
 }
+
+export const calc
