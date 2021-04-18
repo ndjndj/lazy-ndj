06474e36186b431611b4cd141e66cebcb542ee8b
ndjndj <teq.ndj@gmail.com>
2021-04-18 18:58:07 +0900

    fix

--- src/common/common.js
+++ src/common/common.js
@@ -1,9 +1,8 @@
 
 
-export const yaerCalc = (milliSec) => {
+export const calcYear = (milliSec) => {
     const dayMilliSec = 1000 * 60 * 60 * 24;
     const year = 365.25; // うるう年を考慮
 
     return Math.floor((milliSec) / (dayMilliSec) / year);
 }
-
