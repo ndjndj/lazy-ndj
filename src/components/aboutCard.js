2f2d27dde7b94301f2d0c9ec759b26fe00f2f266
ndjndj <teq.ndj@gmail.com>
2021-04-22 17:49:28 +0900

    aboutCard

--- src/components/aboutCard.js
+++ src/components/aboutCard.js
@@ -2,13 +2,26 @@ import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
+const useStyles = makeStyles((theme) => ({
+    sidebarAboutBox: {
+      padding: theme.spacing(2),
+      backgroundColor: theme.palette.grey[200],
+    },
+    sidebarSection: {
+      marginTop: theme.spacing(3),
+    },
+  }));
+
 const AboutCard = () => {
-    <Paper elevation={0} className={classes.sidebarAboutBox}>
-    <Typography variant="h6" gutterBottom>
-        title
-    </Typography>
-    <Typography>description</Typography>
-    </Paper>
+    const classes = useStyles();
+    return (
+        <Paper elevation={0} className={classes.sidebarAboutBox}>
+            <Typography variant="h6" gutterBottom>
+                title
+            </Typography>
+            <Typography>description</Typography>
+        </Paper>
+    );
 }
 
 export default AboutCard;
