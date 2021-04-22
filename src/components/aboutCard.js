b035434521ab50388a97ec70165f97cd8617a246
ndjndj <teq.ndj@gmail.com>
2021-04-22 17:49:49 +0900

    aboutCard

--- src/components/aboutCard.js
+++ src/components/aboutCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     sidebarAboutBox: {
