43428b3544d3728a3531b95c7b6d901a8b170fd3
ndjndj <teq.ndj@gmail.com>
2021-04-22 17:47:44 +0900

    aboutCard

--- src/components/aboutCard.js
+++ src/components/aboutCard.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
 const AboutCard = () => {
-
+    <Paper elevation={0} className={classes.sidebarAboutBox}>
+    <Typography variant="h6" gutterBottom>
+        title
+    </Typography>
+    <Typography>description</Typography>
+    </Paper>
 }
 
 export default AboutCard;
